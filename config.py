d744b420131f6d34b9388fb890619d53b5169ddf
KAIF_CODEC <98528577+kaifcodec@users.noreply.github.com>
2025-04-18 16:53:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
+import os
+
 class Config:
     JSONIFY_PRETTYPRINT_REGULAR = True
-    MAX_CONTENT_LENGTH = 100 * 1024 * 1024  # Limit upload size (100 MB)x:
+    MAX_CONTENT_LENGTH = 100 * 1024 * 1024  # Limit upload size (100 MB)
     DEBUG = True
+
+    # Load cookies from environment variable
+    COOKIES = os.getenv("COOKIES")
